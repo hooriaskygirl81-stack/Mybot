2839c234a02f43d064a64fd21e81a0ce397f2295
hooriaskygirl81-stack <hooriaskygirl81@gmail.com>
2026-06-20 15:28:13 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,15 @@
-bot.py
-TOKEN="8705168654:AAFvFf3iQF1v-r7zMJRRsv5hKZZG8L0isR4"
+import telebot
+
+TOKEN = "8705168654:AAFvFf3iQF1v-r7zMJRRsv5hKZZG8L0isR4"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        "✅ ربات با موفقیت روشن شد."
+    )
+
+print("Bot Started...")
+bot.infinity_polling()
